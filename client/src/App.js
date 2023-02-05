09bb57c9a2eb2f9a6b33f9d1ab5f3e4e806b9d2f
tonycarballeira <tonycarballeira@gmail.com>
2023-02-05 17:02:30 -0500

    using useState and useEffect hooks to show all saved users

---
+++ client/src/App.js
@@ -0,0 +1,33 @@
+import './App.css';
+import { useState, useEffect } from "react";
+import Axios from "axios";
+
+function App() {
+  const [listofUsers, setListOfUsers] = useState([]);
+    //if the app was running online you would replace localhost with the site url
+    
+    useEffect(() => {
+      Axios.get("http://localhost:3001/getUsers").then((response) => {
+        setListOfUsers(response.data);
+      });
+    }, [])
+
+  
+  return (
+    <div className="App">
+      <div className='usersDisplay'>
+        {listofUsers.map((user) => {
+          return (
+            <div>
+              <h1>Name: {user.name}</h1>
+              <h1>Age: {user.age}</h1>
+              <h1>Username: {user.username}</h1>
+            </div>
+          );          
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default App;
--- server/index.js
+++ server/index.js
@@ -3,8 +3,12 @@ const app = express();
 const mongoose = require('mongoose');
 const UserModel = require('./models/Users');
 
+const cors = require("cors");
 // this auto parses json received by requests into objects
 app.use(express.json());
+
+// this helps connect this api with the react frontend without giving errors
+app.use(cors());
 
 mongoose.connect("mongodb+srv://tonycarballeira:Theduke23@cluster0.1of3xbz.mongodb.net/Todo_app?retryWrites=true&w=majority");
 
