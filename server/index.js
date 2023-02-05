92ac18ec17bc7ff751c142b68041bf10c39d5873
tonycarballeira <tonycarballeira@gmail.com>
2023-02-05 16:15:53 -0500

    created route and api endpoint for creating new users

--- server/index.js
+++ server/index.js
@@ -3,6 +3,9 @@ const app = express();
 const mongoose = require('mongoose');
 const UserModel = require('./models/Users');
 
+// this auto parses json received by requests into objects
+app.use(express.json());
+
 mongoose.connect("mongodb+srv://tonycarballeira:Theduke23@cluster0.1of3xbz.mongodb.net/Todo_app?retryWrites=true&w=majority");
 
 app.get("/getUsers", (req, res) => {
@@ -15,6 +18,13 @@ app.get("/getUsers", (req, res) => {
     });
 });
 
+app.post("/createUser", async (req, res) => {
+    const user = req.body;
+    const newUser = new UserModel(user);
+    await newUser.save();
+
+    res.json(user);
+});
 
 app.listen(3001, () => {
     console.log("Server running");
