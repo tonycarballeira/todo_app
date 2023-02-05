79f7a93f9558bd4f848679bd79dac007a4acdbb1
tonycarballeira <tonycarballeira@gmail.com>
2023-02-05 14:59:19 -0500

    created user model

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require('mongoose');
 
-mongoose.connect();
+mongoose.connect("mongodb+srv://tonycarballeira:Theduke23@cluster0.1of3xbz.mongodb.net/Todo_app?retryWrites=true&w=majority");
 
 app.listen(3001, () => {
     console.log("Server running");
