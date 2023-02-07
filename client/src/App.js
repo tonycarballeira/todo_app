b1b9f2e04643d62188235f3b0ee9cd317dde5508
tonycarballeira <tonycarballeira@gmail.com>
2023-02-07 17:26:10 -0500

    created form for adding users and function for sending inputs to api

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,11 @@ import Axios from "axios";
 
 function App() {
   const [listofUsers, setListOfUsers] = useState([]);
+  const [name, setName] = useState("");
+  const [age, setAge] = useState(0);
+  const [username, setUsername] = useState("")
+
+
     //if the app was running online you would replace localhost with the site url
     // this useEffect function makes an api call to the backend
     useEffect(() => {
@@ -12,6 +17,19 @@ function App() {
       });
     }, [])
 
+    const createUser = () => {
+      Axios.post("http://localhost:3001/createUser", {
+        name: name,
+        age: age,
+        username: username
+      }).then ((response) => {
+        setListOfUsers([...listofUsers, {
+          name: name,
+          age: age,
+          username: username
+        }]);
+      });
+    };
   
   return (
     <div className="App">
@@ -25,6 +43,22 @@ function App() {
             </div>
           );          
         })}
+      </div>
+      <div>
+        <input type="text" placeholder='Name...' onChange={(event) => {
+          setName(event.target.value);
+        }}>
+        </input>
+        <input type="Number" placeholder='Age...' onChange={(event) => {
+          setAge(event.target.value);
+        }}>
+        </input>
+        <input type="text" placeholder='Username...' onChange={(event) => {
+          setUsername(event.target.value);
+        }}>
+
+        </input>
+        <button onClick={createUser}>Creat User</button>
       </div>
     </div>
   );
